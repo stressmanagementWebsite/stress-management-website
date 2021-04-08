d548744dcc750b03a1a1901efcd2bf72356792d3
viktoriakomarovskaya <viktoriia.komarovska@sixt.com>
2021-04-08 22:51:23 +0300

    redirect fix

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,40 +1,40 @@
-import React from 'react'
+import React from "react";
 import * as PropTypes from "prop-types";
-import { Link, graphql } from 'gatsby'
-import Masonry from 'react-masonry-component'
-import Img from 'gatsby-image'
-import Layout from "../components/layout"
+import { Router, Redirect } from "@reach/router";
+import Layout from "../components/layout";
 
 const IndexPage = ({ location, data }) => (
-  <Layout>
+  <Redirect to="/en">
+    <Layout>
       <p>This page is blank</p>
-  </Layout>
-)
+    </Layout>
+  </Redirect>
+);
 
-export default IndexPage
+export default IndexPage;
 
 IndexPage.propTypes = {
-    data: {
-        home: {
-            background: {
-                url: PropTypes.string,
-                alt: PropTypes.string,
-            },
-            decorationPicture: {
-                url: PropTypes.string,
-                alt: PropTypes.string,
-            },
-            underline: {
-                url: PropTypes.string,
-                alt: PropTypes.string,
-            },
-            sections: PropTypes.arrayOf(
-                PropTypes.shape({
-                    title: PropTypes.string,
-                    subheadline: PropTypes.string,
-                    paragraph: PropTypes.string
-                })
-            )
-        }
+  data: {
+    home: {
+      background: {
+        url: PropTypes.string,
+        alt: PropTypes.string
+      },
+      decorationPicture: {
+        url: PropTypes.string,
+        alt: PropTypes.string
+      },
+      underline: {
+        url: PropTypes.string,
+        alt: PropTypes.string
+      },
+      sections: PropTypes.arrayOf(
+        PropTypes.shape({
+          title: PropTypes.string,
+          subheadline: PropTypes.string,
+          paragraph: PropTypes.string
+        })
+      )
     }
-}
+  }
+};
